0aff94f82cfd74ae1e5c81accd734b6a08d61e0d
André <andref_lloppnow@outlook.com>
2024-11-25 20:23:37 -0300

    Teste para registrar no site

--- 2-Automacao_com_Cypress/cypress/e2e/Testes_criticos.cy.js
+++ 2-Automacao_com_Cypress/cypress/e2e/Testes_criticos.cy.js
@@ -11,4 +11,15 @@ describe('Testes Criticos DemoBlaze', () => {
       expect(interception.response.statusCode).to.eq(200);
     })
   })
+
+  it('Registrar no site', ()=>{ 
+    cy.visit(URL)
+    cy.get('a[id="signin2"]').click()
+    cy.get('input[id="sign-username"]').type('teste123@teste123.com.br', { force: true }, { delay: 500 })
+    cy.get('input[id="sign-password"]').type('teste123', { delay: 500 })
+    cy.get('button[onclick="register()"]').click()
+    cy.on('window:alert', (alertText) => {
+      expect(alertText).to.contains('Sign up sucessful.');
+    })
+  })
 })
