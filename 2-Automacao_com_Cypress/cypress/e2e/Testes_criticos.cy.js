e0b3abca4efaa6259ae24b55a4d13fb1e5c412cd
Anderson Corrêa <andersoncorrea8001@gmail.com>
2024-11-26 12:39:36 -0300

    Test product add to cart
    small changes on register on site to mage random email and created a new test to verify a item added on cart

--- 2-Automacao_com_Cypress/cypress/e2e/Testes_criticos.cy.js
+++ 2-Automacao_com_Cypress/cypress/e2e/Testes_criticos.cy.js
@@ -1,5 +1,8 @@
+import { faker } from '@faker-js/faker';
+
 describe('Testes Criticos DemoBlaze', () => {
   const URL = 'https://www.demoblaze.com'
+  const randomUsername = faker.internet.email(); // Gera um email aleatório
   
   beforeEach(() => {
     cy.intercept('GET', '/').as('paginaInicial');
@@ -9,17 +12,37 @@ describe('Testes Criticos DemoBlaze', () => {
   it('Acessar o site', () => {
     cy.wait('@paginaInicial').then((interception) => {
       expect(interception.response.statusCode).to.eq(200);
-    })
+    });
   })
 
-  it('Registrar no site', ()=>{ 
-    cy.visit(URL)
-    cy.get('a[id="signin2"]').click()
-    cy.get('input[id="sign-username"]').type('teste123@teste123.com.br', { force: true }, { delay: 500 })
-    cy.get('input[id="sign-password"]').type('teste123', { delay: 500 })
-    cy.get('button[onclick="register()"]').click()
+  it('Registrar no site', () => { 
+
+    // Fluxo do teste
+    cy.get('a[id="signin2"]').click();
+    cy.get('input[id="sign-username"]').type(randomUsername, { force: true, delay: 100});
+    cy.get('input[id="sign-password"]').type('teste123', { delay: 500 });
+    cy.get('button[onclick="register()"]').click();
+
+    // Validar o alerta
     cy.on('window:alert', (alertText) => {
       expect(alertText).to.contains('Sign up sucessful.');
-    })
+    });
   })
+
+  it('Produto no carrinho', () => { 
+    cy.get('a[id="cartur"]').click();
+    cy.get('tbody[id="tbodyid"]').find('tr.sucess').should('have.length', 0);
+
+    cy.get('a[href="index.html"]').first().click();
+    cy.pegarPrimeiroProduto();
+    cy.on('window:alert', (alertText) => {
+      expect(alertText).to.contains('Product added');
+    });
+
+    cy.get('a[id="cartur"]').click();
+    cy.get('tr.success').then((items) => {
+      expect(items.length).to.be.greaterThan(0);
+    });
+  })
+
 })
--- 2-Automacao_com_Cypress/cypress/support/commands.js
+++ 2-Automacao_com_Cypress/cypress/support/commands.js
@@ -7,3 +7,12 @@
 // commands please read more here:
 // https://on.cypress.io/custom-commands
 // ***********************************************
+
+Cypress.Commands.add('pesquisarCategoria', (categoria) => {
+    cy.get(`a[onclick="byCat('${categoria}')"]`).click();
+});
+
+Cypress.Commands.add('pegarPrimeiroProduto', () => {
+   cy.get('a[href^="prod.html?idp"]').first().click();
+   cy.get('a[onclick^="addToCart"]').first().click();
+});
